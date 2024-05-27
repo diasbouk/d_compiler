f0b68ae71e49a49a907c36d801a05c502980e872
diasbouk <boukdsaid@gmail.com>
2024-05-27 06:46:18 +0100

    step one done, first line of ds

--- src/main.cpp
+++ src/main.cpp
@@ -79,6 +79,33 @@ std::vector<Token> tokenizer(const std::string &content) {
   return token;
 }
 
+/* string_to_asm - Converts string to assembly code
+ * @tooken: Token struct that hold some code (line)
+ * Return: A strin (ouput) holding the asm code
+ */
+
+std::stringstream tokens_to_asm(std::vector<Token> token) {
+  std::stringstream output;
+
+  output << "global _start\n_start:\n";
+
+  if (token.size() == 3) {
+    if (token.at(0).type == TokenType::_return)
+      output << "    mov rax, 60\n";
+    if (token.at(1).type == TokenType::int_lat)
+      output << "    mov rdi, " << token.at(1).value.value() << "\n";
+    if (token.at(2).type == TokenType::semicol)
+      output << "    syscall";
+  }
+  return output;
+}
+/* main - Entry point of the programm
+ * @ac: Number of positional passed argumenst
+ * @av: Array of strings containing arguments
+ * Return: As always 0 if it all went good
+ * Or any other integer
+ */
+
 int main(int ac, char **av) {
 
   /* Cheking if the source file exists
@@ -101,19 +128,20 @@ int main(int ac, char **av) {
   buffer << input.rdbuf();
   content = buffer.str();
 
+  /* Tokenizing the content of src file */
   token = tokenizer(content);
 
-  for (int i = 0; i < token.size(); i++) {
-    if (token.at(i).type == TokenType::_return)
-      std::cout << "Return keyword found" << std::endl;
-    if (token.at(i).type == TokenType::int_lat) {
-      std::cout << "Integer value keyword found" << std::endl;
-    }
-    if (token.at(i).type == TokenType::semicol)
-      std::cout << "semicol keyword found" << std::endl;
-  }
-  /* std::cout << content << std::endl; */
-  /* Closing the source file after finishing */
+  /* Converting token to assembly code */
+  std::stringstream asmCode = tokens_to_asm(token);
+
+  /* Moving asm code to file (Hardcoded but we'll fix it later) */
+  std::fstream asmFile("./out.asm", std::ios::out);
+  asmFile << asmCode.str();
+
+  /* std::cout << asmCode.str() << std::endl; */
+  /* Closing the source file after finishing
+   * Should I ? I assume not :_|
+   * */
 
   std::cout << av[1] << std::endl;
   return EXIT_SUCCESS;
